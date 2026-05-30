2182391d29e152f3a2acc560278c05d7838ce0ed
Rey Picazo <emamhdjej@gmail.com>
2026-05-29 18:57:56 -0600

    Navbar funcionando

--- src/components/Nabvar.jsx
+++ src/components/Nabvar.jsx
@@ -1,15 +1,25 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import styles from "./Navbar.module.css"
 
 function Navbar(){
     return (
-        <nav>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/dashboard">Dashboard</Link>
-            <Link to="/editor">Editor</Link>
-            <Link to="/preview">Preview</Link>
+        <nav className={styles.navbar}>
+
+            <Link to="/" className={styles.logo}>
+                Dev<span className={styles.logoSpan}>Profile</span>
+            </Link>
+
+
+            <ul className={styles.links}>
+                <li><NavLink to="/" className={({isActive}) => isActive ? styles.active : ""} end>Inicio</NavLink></li>
+                <li><NavLink to="/about" className={({isActive}) => isActive ? styles.active : ""}>Acerca de</NavLink></li>
+                <li><NavLink to="/dashboard" className={({isActive}) => isActive ? styles.active : ""}>Dashboard</NavLink></li>
+                <li><NavLink to="/editor" className={({isActive}) => isActive ? styles.active : ""}>Editor</NavLink></li>
+                <li><NavLink to="/preview" className={({isActive}) => isActive ? styles.active : ""}>Previsualizar</NavLink></li>
+            </ul>
+
         </nav>
-    )
+    );
 }
 
 export default Navbar
