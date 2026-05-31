4b988c0381ac64753b5d01cb41572d41f4f33ae6
Rey Picazo <emamhdjej@gmail.com>
2026-05-31 08:24:30 -0600

    Pagina about us completa

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,10 +1,40 @@
 function About() {
   return (
-    <div>
-      <h1 className="titulo">Acerca de nosotros</h1>
-      <p className="subtitulo">¿Quienes somos?</p>
+    <div className="about">
+      <div>
+        <h1 className="titulo">Acerca de nosotros</h1>
+        <p className="subtitulo">¿Quiénes somos?</p>
+      </div>
+      <div className="descripcion">
+        <h4>DevProfile es una herramienta diseñada para ayudarte a crear y gestionar tu currículum vitae de manera profesional y eficiente.</h4>
+      </div>
+      <div className="cards">
+        <div className="card">
+          <h6>Objetivo</h6>
+          <p>Ayudar a los usuarios a crear y gestionar su currículum vitae de manera profesional y eficiente.</p>
+        </div>
+        <div className="card">
+          <h6>Visión</h6>
+          <p>Ser la plataforma líder en creación y gestión de currículum vitae, reconocida por su facilidad de uso, personalización y calidad profesional.</p>
+        </div>
+        <div className="card">
+          <h6>Misión</h6>
+          <p>Proporcionar a los usuarios una herramienta intuitiva y accesible para crear currículum vitae profesionales que les permitan destacar en el mercado laboral.</p>
+        </div>
+        <div className="card">
+          <h6>Valores</h6>
+          <p>Innovación, calidad, accesibilidad, personalización, satisfacción del cliente.</p>
+        </div>
+        <div className="card">
+          <h6>Privacidad</h6>
+          <p>Nos comprometemos a proteger la privacidad de nuestros usuarios y garantizar la seguridad de sus datos personales.</p>
+        </div>
+        <div className="card">
+          <h6>Contacto</h6>
+          <p>Si tienes alguna pregunta o necesitas ayuda, no dudes en contactarnos: rincontesoros@gmail.com</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
-
-export default About
+export default About;
