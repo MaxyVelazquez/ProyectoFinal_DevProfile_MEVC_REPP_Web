bd98ce26ff67f0965f31d534e85f2509abff0c3d
MaxyVelazquez <maxyevc@gmail.com>
2026-06-05 21:26:02 -0600

    fix:consistencia en dashboard cuando hay errores en datos

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,7 +1,35 @@
 import Resumen from "../components/Resumen";
-
+import Swal from "sweetalert2";
+import { useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { CVContext } from "../context/CVContext";
 
 function Dashboard() {
+
+  const navigate = useNavigate();
+    const contextValue = useContext(CVContext);
+    const {datosPersonales, } = contextValue;
+
+  useEffect(() => {
+      const camposFaltantes = [];
+  
+      if (!datosPersonales.nombre?.trim()) camposFaltantes.push(1);
+      if (!datosPersonales.carrera?.trim()) camposFaltantes.push(1);
+      if (!datosPersonales.pais?.trim()) camposFaltantes.push(1);
+      if (!datosPersonales.estado?.trim()) camposFaltantes.push(1);
+      if (!datosPersonales.correo?.trim()) camposFaltantes.push(1);
+      if (!datosPersonales.descripcion?.trim()) camposFaltantes.push(1);
+  
+      if (camposFaltantes.length > 0) {
+        Swal.fire({
+          icon: "warning",
+          title: "Datos incompletos",
+          html: `Hay informacion incompleta, favor de completar`,
+          confirmButtonText: "Seguir editando",
+          confirmButtonColor: "rgba(182, 0, 0, 1)",
+        }).then(() => navigate("/editor"));
+      }
+    }, []);
   return (
     <div className="fondoNegro">
       <div className="dashboard-container">
